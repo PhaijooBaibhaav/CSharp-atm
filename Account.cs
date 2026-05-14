7ec5656f5bc01f970e067d42a27a3c67563aa5bd
B41BH4V <baibhavphaijoo@gmail.com>
2026-05-14 19:36:54 +0545

    add methods and variables to class Account

--- Account.cs
+++ Account.cs
@@ -1,4 +1,22 @@
 using System; 
+
 class Account {
+
+    double balance;
+    string name;
+    int pinCode;
+
+    public void withdrawmoney(double amount) {
+        balance -= amount;
+    }
+
+    public void depositmoney(double amount) {
+        balance += amount;
+    }
+
+    public double GetBalance() {
+        return balance;
+    }
+
 
 }
