2d10afc864466cfc4743f438496273b8e8caa293
B41BH4V <baibhavphaijoo@gmail.com>
2026-05-15 07:28:18 +0545

    add method signup

--- ATM.cs
+++ ATM.cs
@@ -1,13 +1,27 @@
 using System;
 using System.Collections.Generic;
 
-Account account = new Account();
-
 class ATM {
 
     private List<Account> accounts = new List<Account>();
+
+    int pin; 
+    string name; 
+
     public void SignUp() {
 
+        Console.WriteLine("Write your name: ");
+        name = Console.ReadLine();
+
+        Console.WriteLine("Write your pin code: ");
+        pin = Convert.ToInt32(Console.ReadLine());
+
+        Account account = new Account();
+
+        account.name = name;
+        account.pinCode = pin;
+
+        accounts.Add(account);
     }
 
     public void Login() {
