90ac52ee3e89ddd0985da0e6507517648b35a6b4
B41BH4V <baibhavphaijoo@gmail.com>
2026-05-15 06:44:29 +0545

    add account list and signup and login methods, not complete

--- ATM.cs
+++ ATM.cs
@@ -1,5 +1,16 @@
 using System;
+using System.Collections.Generic;
+
+Account account = new Account();
 
 class ATM {
-    
-}
+
+    private List<Account> accounts = new List<Account>();
+    public void SignUp() {
+
+    }
+
+    public void Login() {
+
+    }
+}    
