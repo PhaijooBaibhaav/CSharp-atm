ef0ccbce532a3fe818f3b5adb880d0ad8f22e69b
B41BH4V <baibhavphaijoo@gmail.com>
2026-05-16 08:55:34 +0545

    create saving and loading files|

--- FileManager.cs
+++ FileManager.cs
@@ -4,4 +4,45 @@ using System.Collections.Generic;
 
 class FileManager {
 
+
+    private static string path = "data/accounts.txt";
+
+    public static void SaveData(List<Account> accounts) {
+
+        StreamWriter writer = new StreamWriter(path);
+
+            foreach (Account account in accounts)
+            {
+                writer.WriteLine(
+                    $"{account.name},{account.pinCode},{account.GetBalance()}"
+                );
+            }
+            
+        writer.Close();
+    }
+
+    public static List<Account> LoadData() {
+
+        List<Account> accounts = new List<Account>();
+        if (!File.Exists(path)) {
+            return accounts;
+        }
+
+        string[] lines = File.ReadAllLines(path);
+
+        foreach (string line in lines) {
+            string[] data = lines.Split(',');
+
+            Account account = new Account();
+
+            account.name = data[0];
+            account.pinCode = Convert.ToInt32(data[1]);
+            double balance = Convert.ToDouble(data[2]);
+            account.DepositMoney(balance);
+
+            accounts.Add(account);
+        }
+
+        return accounts;
+    }
 }
