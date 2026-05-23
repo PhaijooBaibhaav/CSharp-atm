6b003b88e1e79a0270daf8dc75cadae3da6fc4ca
B41BH4V <baibhavphaijoo@gmail.com>
2026-05-23 09:34:01 +0545

    complete program.cs

--- Program.cs
+++ Program.cs
@@ -3,5 +3,13 @@
 class Program {
     static void Main(String[] args) {
 
+        List<Account> accounts = FileManager.LoadData();
+        ATM atm = new ATM(accounts);
+
+        atm.SignUp();
+        atm.Login();
+
+        FileManager.SaveData(accounts);
+
     }
 }
